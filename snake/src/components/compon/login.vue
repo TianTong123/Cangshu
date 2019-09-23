bbf3db2b072422abcb3ee1f86ff441a6f8e7fb8f
16210120411 <16210120411@qq.com>
2019-09-23 15:49:15 +0800

    登录页

--- snake/src/components/compon/login.vue
+++ snake/src/components/compon/login.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>dededede</h1>
+  <div class="login">
+    <div class="wrap">
+      <div class="bg-music"><video src=""></video></div>
+      <div class="logo"></div>
+      <div class="login-wrap">
+        <div class="inp"><input type="text"></div>
+        <div class="inp"><input type="text"></div>
+        <div class="btn">登录</div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +17,5 @@ export default {
 }
 </script>
 <style scoped>
-
+@import '../../../static/css/login.css'
 </style>
