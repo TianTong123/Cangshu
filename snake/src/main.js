0e3a681d6b973cb008575d7a23f22a47a761327d
16210120411 <16210120411@qq.com>
2019-09-23 15:06:26 +0800

    新增了点东西

--- snake/src/main.js
+++ snake/src/main.js
@@ -3,13 +3,11 @@ import App from './App'
 import router from './router'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
-import httpService from './service/service';//封装axios接口强求/*
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 axios.defaults.baseURL = '/api'//改成/api才能用proxyTable跨域,反正这个就是把那个瞎写的路径配进去的实现
-Vue.prototype.$axios = httpService; //新增，有影响就删除 
 Vue.prototype.$ajax = axios
 
 new Vue({
--- snake/src/router/index.js
+++ snake/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 
 Vue.use(Router);
@@ -9,8 +8,24 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: '跳转首页',
+      redirect: '/snake/login',
+    },
+    {
+      path: '/snake',
+      name: '跳转首页',
+      redirect: '/snake/index',
+      children: [
+      
+      ]
+    },
+    {
+      path: '/snake/login',
+      name: '登录',
+      meta: {
+         keepAlive: true // 需要被缓存 
+      },
+      component: resolve => require(['../components/compon/login.vue'], resolve)
+    },
   ]
 })
